8632c79c8a5e80932ff3ba44c2b54a35e1ba4ce3
Hugo Averty <hugoaverty@gmail.com>
2025-01-28 10:58:28 +0100

    Fixing roof ? adding start zone, adding lights

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,22 @@ WA.onInit().then(() => {
     console.log('Player tags: ',WA.player.tags)
 
     WA.room.onEnterLayer("roofZone").subscribe(() => {
-        WA.room.hideLayer('roof');
-        WA.room.hideLayer('roof2');
+        WA.room.hideLayer('Roof');
+        WA.room.hideLayer('Roof2');
     });
+
     WA.room.onLeaveLayer("roofZone").subscribe(() => {
-        WA.room.showLayer('roof');
-        WA.room.showLayer('roof2');
+        WA.room.showLayer('Roof1');
+        WA.room.showLayer('Roof2');
+    });
+
+    WA.ui.actionBar.addButton({
+        id: 'explore-btn',
+        // @ts-ignore
+        label: 'Event info',
+        callback: () => {
+            WA.nav.openCoWebSite("https://workadventure.github.io/embed-pages/src/pasqal/welcome.html");
+        }
     });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
