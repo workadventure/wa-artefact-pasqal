b8d7463ed686a6af1047b4e4e20a2ba916728722
Hugo Averty <hugoaverty@gmail.com>
2025-02-04 17:00:39 +0100

    Improving stuff

--- src/main.ts
+++ src/main.ts
@@ -26,47 +26,69 @@ WA.onInit().then(() => {
         WA.room.hideLayer('silentRoof');
     });
 
+    WA.ui.actionBar.removeButton("invite");
+    WA.controls.disableInviteButton();
+
+
     let ressourcesUrl: any;
+    let githubUrl: any;
     if(WA.player.tags.includes("room1")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1CDLSMttrmndpWNLSky7pFvGeTflR5yLp?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room2")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1937ac3i5jZsPISRolIJuWt3-31s1x1pH?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room3")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1s65iaJEptQyCEl-mf9vUh8xQuYH91J9n?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room4")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1t_ocFUi-F8aIyJAH5F2JiHOn2MC_dvvZ?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room5")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1zVUnAdlHFvChsI69dvXiLu8k8SY8Zlwp?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room6")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1Bm32-5zUX2jqYosLZv6KzRdqL1xZa2gv?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room7")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/17dXMnrooA1c99SN0hcYORNMdHEB_K55T?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room8")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1OkBsinwvNWJtg7nnSAZ4EJ3KFkjdmB7F?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room9")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/14sDTehs-_Tjzh7dO1A_YYKlnkaiGAHzd?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room10")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1AI85Rc0F5SMRY_T2NQUXV9kQy22Rbzl7?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room11")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1bvw5m6WCwzVfYAXgyFqF7stp5IhVB3Cr?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room12")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/10GJl9QJ73wr1jp0irPzn4bulh_vUMU-3?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room13")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1zgWxsDV3SHGJNHeIFlp7nsr-GfNHpp3W?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room14")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1BJxMu1A434jx8Jws2RNy6fDq1mWKRxCD?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("room15")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1XWV9m0RSM-oOUFp3ZN0-DdbMpZgeeqDZ?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     } else if (WA.player.tags.includes("admin")) {
         ressourcesUrl = "https://drive.google.com/drive/folders/1b7-l8iWaZlQFkuqwATrP9SFlkqFVv8HC?usp=drive_link";
+        githubUrl = "https://github.com/workadventure/workadventure";
     }
 
     WA.ui.actionBar.addButton({
-        id: 'help-btn',
-        // @ts-ignore
-        label: 'Help',
+        id: 'github-btn',
+        type: 'action',
+        imageSrc: 'https://www.shareicon.net/data/256x256/2015/09/15/101512_logo_512x512.png',
+        toolTip: 'Github project repository',
         callback: () => {
-            WA.nav.openCoWebSite("https://workadventure.github.io/embed-pages/src/pasqal/welcome.html");
+            WA.nav.openTab(githubUrl as string);
         }
     });
 
@@ -74,9 +96,18 @@ WA.onInit().then(() => {
         id: 'workspace',
         // @ts-ignore
         label: 'Ressources',
-        toolTip: 'Access to your project directory',
+        toolTip: 'Access to your project directory<style>#workspace {background: red !important;}</style>',
         callback: () => {
             WA.nav.openTab(ressourcesUrl as string);
+        }
+    });
+
+    WA.ui.actionBar.addButton({
+        id: 'help-btn',
+        // @ts-ignore
+        label: 'Help',
+        callback: () => {
+            WA.nav.openCoWebSite("https://workadventure.github.io/embed-pages/src/pasqal/welcome.html");
         }
     });
     let helpZone: any;
